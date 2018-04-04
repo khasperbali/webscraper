c21c03f9a34437c99486eafb01224b135b92af9a
Aoiryuu <19joroka@waipahuh.k12.hi.us>
2018-04-04 12:28:05 -1000

    NEEDS WORK

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# webscraper
+BitCoin Webscraper
+This is a webscraper. 
+This will find the price of bitcoin to usd for you. 
+To use it simply
+1)Clone the file
+2)run "python webscraper.py" in a terminal and it'll run.
--- webscaper.py
+++ webscaper.py
@@ -1,7 +1,15 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
-def scraper(url): {
-	soup = BeautifulSoup(page.content, html.parser);
-	print(soup.prettify());
-}
+def scrapePrice():
+    url = "https://coinmarketcap.com/currencies/bitcoin/"
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page, "html.parser")
+    name_box = strftime("As Of " + "%m/%d/%Y %H:%M", gmtime()) + ", the Bitcoin price is" + " $" + soup.find(attrs={"class": "text-large2"}).get_text()
+    text = name_box.encode()
+    print(text)
+    f = open('bitcoinPrice.txt', 'wb')
+    f.write(text)
+    f.close()
+    print('Price saved to bitcoinPrice.txt')
+    
